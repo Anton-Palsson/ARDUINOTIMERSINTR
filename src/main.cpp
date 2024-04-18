d21de8fccc59f7cc6c399819329e1d2764976100
Anton-Palsson <antonericpalsson@gmail.com>
2024-04-18 10:43:14 +0200

    a

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,35 @@
-#include <Arduino.h>
+#include <avr/io.h>
+#include <avr/interrupt.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#define LED_PIN (1 << PD3)
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+void toggleLed() {
+    PORTD ^= LED_PIN; // Toggle LED
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void interruptSetup() {
+    DDRD |= LED_PIN;
+
+    TCCR1A = 0; // Init Timer1
+    TCCR1B = 0; // Init Timer1
+    TCCR1B |= (1 << CS11) | (1 << CS10); // Prescaler = 64
+    OCR1A = 31250; // Timer Preloading
+    TIMSK1 |= (1 << OCIE1A); // Enable Timer Compare A Match Interrupt
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+ISR(TIMER1_COMPA_vect) {
+    toggleLed();
 }
+
+int main() {
+    interruptSetup();
+    sei(); // Enable int viktig!!
+
+    while (1) {
+        // måste loopa
+    }
+
+    return 0;
+}
+
+
